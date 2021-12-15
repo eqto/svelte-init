e24b262e12428dbcd6950801df7f69047b60fd7d
eqto <e.yulian@gmail.com>
2021-12-15 10:01:11 +0700

    fix theme + dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,13 @@ const config = {
 		}),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+
+		// vite: {
+		// 	ssr: {
+		// 		noExternal: ['/^@material//', '/^@smui(?:-extra)?//']
+		// 	},
+		// },
 	}
 };
 
