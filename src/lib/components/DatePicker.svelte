bd2e6b8f2e6220e1dd7a5a89d2b14ee16b64ed31
eqto <e.yulian@gmail.com>
2021-12-22 20:29:43 +0700

    datepicker

---
+++ src/lib/components/DatePicker.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    export let fromDate, untilDate;
+    import flatpickr from "flatpickr";
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        flatpickr("#startDate", {});
+        flatpickr("#endDate", {});
+    });
+</script>
+
+<div class="date">
+    <div>Tanggal:</div>
+    <div class="item">
+        <input id="startDate" bind:value={fromDate} /> s/d
+        <input id="endDate" bind:value={untilDate} />
+    </div>
+</div>
+
+<style lang="scss">
+    #startDate,
+    #endDate {
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        padding: 4px;
+        text-align: center;
+        width: 128px;
+    }
+    div.date {
+        margin: 4px 0;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        & > div {
+            margin: 0 4px;
+        }
+    }
+
+    div.filter {
+        display: flex;
+        margin-top: 16px;
+        flex-direction: row;
+        justify-content: center;
+        & > div {
+            margin: 0 4px;
+        }
+    }
+</style>
--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -1,45 +1,157 @@
 <script lang="ts">
+    import DatePicker from "$lib/components/DatePicker.svelte";
     import { post } from "$lib/http";
+    import Button, { Label } from "@smui/button";
 
-    import { onMount } from "svelte";
+    let items = [];
+    let totalCount = 0;
+    let totalPrice = 0,
+        totalQuantity = 0;
 
-    let data;
+    const date = new Date();
+    let fromDate =
+        date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+    let untilDate =
+        date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
 
-    post("/api/package").then((resp) => {
-        if (resp.status == 0) data = resp.data;
-    });
+    function search() {
+        post("/api/package", { from: fromDate, until: untilDate }).then(
+            (resp) => {
+                if (resp.status == 0 && resp.data != null) {
+                    const data = resp.data as any;
+
+                    data.items.forEach((el) => {
+                        const item = el;
+                        item["sender"] = JSON.parse(el["sender"]);
+                        item["receiver"] = JSON.parse(el["receiver"]);
+                        items = [...items, item];
+                    });
+                    totalCount = data.total_count;
+                    totalPrice = data.total_price;
+                    totalQuantity = data.total_quantity;
+                }
+            }
+        );
+    }
+
+    function calcInsurance(price: number) {
+        if (price >= 75000 && price < 5000000) return "Qoala";
+        else if (price >= 500000) return "Paspol";
+        return "";
+    }
 </script>
 
-{#if data != null}
+<div class="date">
+    <div>
+        <DatePicker bind:fromDate bind:untilDate />
+    </div>
+    <Button variant="raised" on:click={search}><Label>Search</Label></Button>
+
+    <div class="total">
+        <div>Count: {totalCount}</div>
+        <div>Quantity: {totalQuantity}</div>
+        <div>Price: {totalPrice}</div>
+    </div>
+</div>
+
+{#if items.length > 0}
     <table>
         <tr>
             <th>Parter TrxID</th>
-            <th>Date</th>
             <th>Product Name</th>
+            <th>Product ID</th>
+            <th>Qty</th>
+            <th>Package Type</th>
             <th>Price</th>
             <th>Sender Name</th>
-            <th>Sender Province</th>
+            <th>Sender Phone</th>
+            <th>Sender Email</th>
+            <th>Origin Address</th>
+            <th>Origin District</th>
+            <th>Origin City</th>
+            <th>Origin Province</th>
+            <th>Origin Country</th>
+            <th>Origin Zipcode</th>
             <th>Receiver Name</th>
-            <th>Receiver Province</th>
+            <th>Receiver Phone</th>
+            <th>Receiver Email</th>
+            <th>Destination Address</th>
+            <th>Destination District</th>
+            <th>Destination City</th>
+            <th>Destination Province</th>
+            <th>Destination Country</th>
+            <th>Destination Zipcode</th>
+            <th>Shipment Date</th>
+            <th>Shipment Code</th>
+            <th>Shipment Cost</th>
             <th>Transporter</th>
+            <th>Driver Name</th>
+            <th>Cargo Type</th>
+            <th>Asuransi</th>
         </tr>
-        {#each data as item}
+        {#each items as item}
             <tr>
                 <td>{item.partner_trx_id}</td>
-                <td>{item.date}</td>
                 <td>{item.product_name}</td>
+                <td>{item.product_id}</td>
+                <td class="number">{item.quantity}</td>
+                <td>{item.package_type}</td>
                 <td class="number">{item.product_price}</td>
-                <td>{item.sender_name}</td>
-                <td>{item.sender_province}</td>
-                <td>{item.receiver_name}</td>
-                <td>{item.sender_province}</td>
+                <td>{item.sender.name}</td>
+                <td>{item.sender.phone}</td>
+                <td>{item.sender.email}</td>
+                <td>{item.sender.address}</td>
+                <td>{item.sender.district}</td>
+                <td>{item.sender.city}</td>
+                <td>{item.sender.province}</td>
+                <td>{item.sender.country}</td>
+                <td>{item.sender.postal_code}</td>
+                <td>{item.receiver.name}</td>
+                <td>{item.receiver.phone}</td>
+                <td>{item.receiver.email}</td>
+                <td>{item.receiver.address}</td>
+                <td>{item.receiver.district}</td>
+                <td>{item.receiver.city}</td>
+                <td>{item.receiver.province}</td>
+                <td>{item.receiver.country}</td>
+                <td>{item.receiver.postal_code}</td>
+                <td>{item.date}</td>
+                <td>{item.shipment_code}</td>
+                <td>{item.shipment_cost}</td>
                 <td class="center">{item.shipment_type}</td>
+                <td>{item.driver_name}</td>
+                <td>{item.cargo_type}</td>
+                <td>{calcInsurance(item.product_price)}</td>
             </tr>
         {/each}
+        <tr>
+            <td colspan="4" class="number">{totalQuantity}</td>
+            <td colspan="2" class="number">{totalPrice}</td>
+            <td colspan="23" />
+        </tr>
     </table>
 {/if}
 
 <style lang="scss">
+    div.date {
+        padding: 8px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+
+        & .total {
+            display: flex;
+            flex-direction: row;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-shadow: 0 0 4px #ccc;
+            padding: 8px;
+            margin-left: 16px;
+            & > div {
+                margin: 5px;
+            }
+        }
+    }
     table {
         font-size: 0.8em;
         border-spacing: 0;
@@ -47,6 +159,10 @@
         & th {
             border-top: 1px solid #ccc;
             border-bottom: 1px solid #ccc;
+        }
+
+        & td {
+            border-bottom: 1px solid #eee;
         }
 
         & tr:last-child td {
